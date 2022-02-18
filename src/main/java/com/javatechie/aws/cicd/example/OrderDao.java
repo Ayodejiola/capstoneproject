35248392edb7c511067a99b61a5432d7d5767dac
Ayodejiola <97601366+Ayodejiola@users.noreply.github.com>
2022-02-18 16:58:31 +0100

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -12,7 +12,7 @@ public class OrderDao {
 
     public List<Order> getOrders() {
         return Stream.of(
-                new Order(1000, "Mobile", 1, 220000),
+                new Order(1000, "Mobile", 1, 10000),
                 new Order(58, "Book", 4, 3000),
                 new Order(205, "Laptop", 1, 150000),
                 new Order(809, "headset", 1, 1799))
